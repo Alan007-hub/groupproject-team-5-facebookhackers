d1e0867430be36743c8d8281fdc642e2ac5f331e
Aung Kaung Khant <a_khant@u.pacific.edu>
2020-10-30 10:21:05 -0700

    Initial code in Spawn Point

--- group-project/src/starter/SpawnPoint.java
+++ group-project/src/starter/SpawnPoint.java
@@ -1,5 +1,27 @@
 package starter;
 
 public class SpawnPoint {
+private int yPosition;
+private int xPosition;
+
+void setX(int x) {
+	xPosition=x;
+	
+}
+
+
+void setY(int y) {
+	yPosition=y;
+}
+	
+
+int getX() {
+	return xPosition;
+}
+
+int getY() {
+	return yPosition;
+}
+
 
 }
