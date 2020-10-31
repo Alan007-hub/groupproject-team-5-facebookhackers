cb2c181a47c3ddad9fb93fe14734f708fae8ed46
Jorge Zepeda <j_zepeda3@u.pacific.edu>
2020-10-30 19:45:21 -0700

    building the leaderboard class

--- group-project/src/starter/Leaderboard.java
+++ group-project/src/starter/Leaderboard.java
@@ -1,5 +1,44 @@
 package starter;
+import java.io.File;
+import java.util.ArrayList;
+
+
 
 public class Leaderboard {
-	//Testing commit again!
+	
+	private static Leaderboard board;
+	private String filePath;
+	private String highScores;
+	
+	private ArrayList<Integer> topScores;
+	
+	private Leaderboard() {
+		filePath = new File("").getAbsolutePath();
+		highScores = "Scores";
+		topScores = new ArrayList<Integer>();
+	}
+	
+	public static Leaderboard newInstance() {
+		if (board == null) {
+			board = new Leaderboard();
+		}
+		return board; 
+	}
+	
+	public void addScore(int score) {
+		for(int i = 0; i < topScores.size(); i++) {
+			if(score >= topScores.get(i)) {
+				topScores.add(i, score);
+				topScores.removve(topScores.size()-1);
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
+
