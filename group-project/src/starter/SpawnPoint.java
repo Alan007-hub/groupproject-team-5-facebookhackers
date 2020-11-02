7e82361566f59bfd21ccac1738e6cb84ed6a7f61
michael guerra <m.guerra809@gmail.com>
2020-11-01 20:19:11 -0800

    Added default constructor and constructor for two arguments

--- group-project/src/starter/SpawnPoint.java
+++ group-project/src/starter/SpawnPoint.java
@@ -3,25 +3,29 @@ package starter;
 public class SpawnPoint {
 	private int yPosition;
 	private int xPosition;
-
+//Default Constructor
+	SpawnPoint(){
+		yPosition = 0;
+		xPosition = 0;
+	}
+//Constructor for passing two arguments
+	SpawnPoint(int x, int y){
+		xPosition = x;
+		yPosition = y;
+	}
+	
+//Setters and getters
 	void setX(int x) {
 		xPosition = x;
 
 	}
-
-
 	void setY(int y) {
 		yPosition = y;
 	}
-
-
 	int getX() {
 		return xPosition;
 	}
-
 	int getY() {
 		return yPosition;
 	}
-
-
 }
