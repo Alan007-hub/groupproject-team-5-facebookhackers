89cba876a8cd8a7db1ff0246650cb53489d411d3
michael guerra <m.guerra809@gmail.com>
2020-11-01 20:47:32 -0800

    Added constructor,getters, and setters.

--- group-project/src/starter/Mole.java
+++ group-project/src/starter/Mole.java
@@ -1,6 +1,33 @@
 package starter;
 
+import acm.graphics.GImage;
+
 public class Mole{
 		private SpawnPoint spawn;
+		private GImage mole;
 		
+		//Default constructor +2 more
+		Mole(){
+			spawn = new SpawnPoint();
+			mole = new GImage("mole.jpg",spawn.getX(),spawn.getY());
+		}
+		Mole(SpawnPoint spawn){
+			this.spawn = spawn;
+			mole = new GImage("mole.jpg",spawn.getX(),spawn.getY());
+		}
+		Mole(int x, int y){
+			spawn = new SpawnPoint(x,y);
+			mole = new GImage("mole.jpg",spawn.getX(),spawn.getY());
+		}
+		
+		//Getters and Setters
+		SpawnPoint getSpawn() {
+			return spawn;
+		}
+		GImage getMoleImage() {
+			return mole;
+		}
+		void setSpawn(SpawnPoint spawn) {
+			this.spawn = spawn;
+		}
 }
