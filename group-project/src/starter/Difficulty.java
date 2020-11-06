5f81e7fd5f401aa4b71e8d42a068fa4aa2f60989
michael guerra <m.guerra809@gmail.com>
2020-11-06 10:26:00 -0800

    Created and completed difficulty.java

---
+++ group-project/src/starter/Difficulty.java
@@ -0,0 +1,35 @@
+package starter;
+
+
+//Intended to work with the graphicsDiffcultySelect.java. When the user selects a difficulty it will create an instance of this class
+public class Difficulty {
+	private int difficultyLevel;
+	private String nameOfDiffculty[];
+	
+	Difficulty(){
+		difficultyLevel = 1;
+		nameOfDiffculty = new String[4];
+		nameOfDiffculty[0] = "Easy";
+		nameOfDiffculty[1] = "Medium";
+		nameOfDiffculty[2] = "Hard";
+		nameOfDiffculty[3] = "Impossible";
+	}
+	Difficulty(int value){
+		difficultyLevel = value;
+		nameOfDiffculty = new String[4];
+		nameOfDiffculty[0] = "Easy";
+		nameOfDiffculty[1] = "Medium";
+		nameOfDiffculty[2] = "Hard";
+		nameOfDiffculty[3] = "Impossible";
+	}
+	
+	void setDiffcultyLevel(int value) {
+		difficultyLevel = value;
+	}
+	int getDiffcultyLevel() {
+		return difficultyLevel;
+	}
+	String getNameOfDiffculty() {
+		return nameOfDiffculty[difficultyLevel];
+	}
+}
