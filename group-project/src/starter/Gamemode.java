79995bad931361ef33aea7a433b4b1e95b7b30e6
michael guerra <m.guerra809@gmail.com>
2020-11-07 15:29:21 -0800

    Added constructors

--- group-project/src/starter/Gamemode.java
+++ group-project/src/starter/Gamemode.java
@@ -1,5 +1,14 @@
 package starter;
 
-public class Gamemode {
+public class Gamemode{
+	private Difficulty currentDLevel;
+	private Mole molesInHoles[];
 	
+	//Constructors for Gamemode
+	Gamemode(){
+		currentDLevel = new Difficulty();
+	}
+	Gamemode(Difficulty d){
+		currentDLevel = d;
+	}
 }
